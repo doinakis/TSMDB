0eb890d23c2931f04f2dd49a4d066d3e97b0a239
Doinakis <doinakis.michalis@gmail.com>
2021-01-08 16:53:55 +0200

    ADD: templates

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask, escape, request, send_file, render_template
+from flask import Flask, escape, request, send_file, render_template, redirect, url_for
 import MySQLdb
-
 # SECTION: Constants
 # Flags if on HEROKU environment
 ON_HEROKU = 'IS_HEROKU' in os.environ
@@ -14,7 +13,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/', methods=[ 'GET' ])
 def get_index():
     """Redirects default page to login.html"""
-    return render_template('login.html', mimetype='text/html')
+    return render_template('index.html', info = {"name":"kappa","id":1})
 
 @app.route('/images/<filename>', methods=[ 'GET' ])
 def get_image(filename):
@@ -28,6 +27,25 @@ def get_image(filename):
 def get_style():
     """Redirects styles.css to the default css file"""
     return send_file('templates/styles.css', mimetype='text/css')
+
+@app.route('/login', methods=[ 'GET' ])
+def get_login():
+    return render_template('login.html')
+
+@app.route('/login', methods=[ 'POST' ])
+def post_login():
+    error = None
+    if valid_login(request.form['username'], request.form['password']):
+        return send_file('templates/index.html', mimetype='text/html')
+    else:
+        error = 'Invalid username/password'
+    return render_template('login.html', error=error)
+
+def valid_login(username, password):
+    if (username == 'Michalis') and (password == 'Doinakis'):
+        return True
+    else:
+        return False 
 
 
 # SECTION: Main
